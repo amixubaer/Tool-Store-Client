d0648b76bb15f08e0bbf365d7c989830d8476180
Abu Jubaer <abujubaer90@gmail.com>
2022-05-25 00:39:26 +0600

    add order

--- src/Pages/Homepage/Home/Home.jsx
+++ src/Pages/Homepage/Home/Home.jsx
@@ -1,20 +1,26 @@
 import React from "react";
-import Footer from "../../Shared/Footer/Footer";
+import Button from "@mui/material/Button";
 import Products from "../../Products/Products";
-import Header from "../../Shared/Header/Header";
 import Category from "../Category/Category";
 import Review from "../Review/Review";
 import Slider from "../Slider/Slider";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <Header />
+      
       <Slider />
       <Category />
-      <Products quantity={9} />
+      <Products quantity={6} />
+      <div className='text-center'>
+        <Link to='/products' style={{ textDecoration: "none" }}>
+          <Button variant='contained' color='secondary'>
+            Explore All Products
+          </Button>
+        </Link>
+      </div>
       <Review />
-      <Footer />
     </div>
   );
 };
